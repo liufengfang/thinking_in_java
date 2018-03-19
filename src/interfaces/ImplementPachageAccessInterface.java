7bd09f651965eb5f245cd1c13a1bd4912aa86590
liufengfang <429159575@qq.com>
2018-03-19 23:51:14 +0800

    章节9.2 接口 只有包访问权限的接口的方法同样只能是public的

---
+++ src/interfaces/ImplementPachageAccessInterface.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+/**
+ * Created by dulili on 2018/3/19.
+ */
+public class ImplementPachageAccessInterface implements InterfaceWithPackageAccess {
+    //不能是protected或private
+    @Override
+    public void DefautlRightMethod() {
+
+    }
+}
---
+++ src/interfaces/TestInterfaceWithPackageAccess.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by dulili on 2018/3/19.
+ */
+public class TestInterfaceWithPackageAccess {
+}
+
+
+interface InterfaceWithPackageAccess{
+    //默认是也只能是public访问权限
+    void DefautlRightMethod();
+}
