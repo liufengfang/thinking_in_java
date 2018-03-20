c34d8d447d24b52a2d75d4957da643752b055997
liufengfang <429159575@qq.com>
2018-03-20 22:23:39 +0800

    类名优化

--- src/interfaces/ImplementPackageAccessInterface.java
+++ src/interfaces/ImplementPackageAccessInterface.java
@@ -3,7 +3,7 @@ package interfaces;
 /**
  * Created by dulili on 2018/3/19.
  */
-public class ImplementPachageAccessInterface implements InterfaceWithPackageAccess {
+public class ImplementPackageAccessInterface implements InterfaceWithPackageAccess {
     //不能是protected或private
     @Override
     public void DefautlRightMethod() {
